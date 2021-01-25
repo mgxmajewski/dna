0a59c147de59959c4727a9ca65726049a46675ac
mgxmajewski <mgxmajewski@gmail.com>
2021-01-25 23:20:03 +0000

    Add list with only unique sequences

--- dna.py
+++ dna.py
@@ -6,24 +6,27 @@ material = sys.argv[1]
 dna = []
 
 with open (material, "r") as f:
-    reader = csv.DictReader(f)
+    reader = csv.reader(f)
     for nucleotide in f:
         dna.append(nucleotide)
 
-
 sequences = []
 
 i = 0
-
-
-sequences = []
-
-i = 0
-
 
 for i in range (i, len(nucleotide), i+4):
     sequences.append(nucleotide[i:i+4])
     i += 1
+    
 
+repeated = []
 
-print(sequences)
+x = len(sequences)
+for k in range(x):
+    if sequences[k] not in repeated:
+        repeated.append(sequences[k])
+    
+print(len(repeated))
+print(len(sequences))
+
+        
