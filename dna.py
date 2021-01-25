e57c8297ee8ae9e87d2aa99d6501643229a6acf5
mgxmajewski <mgxmajewski@gmail.com>
2021-01-25 23:43:32 +0000

    Read people database into memory

--- dna.py
+++ dna.py
@@ -1,11 +1,22 @@
 import sys
 import csv
 
-material = sys.argv[1]
+
+people_database = sys.argv[1]
+
+people = []
+
+with open (people_database, "r") as f:
+    people_reader = csv.DictReader(f)
+    for person in f:
+        people.append(person)
+
+
+dna_material = sys.argv[2]
 
 dna = []
 
-with open (material, "r") as f:
+with open (dna_material, "r") as f:
     reader = csv.reader(f)
     for nucleotide in f:
         dna.append(nucleotide)
@@ -19,14 +30,19 @@ for i in range (i, len(nucleotide), i+4):
     i += 1
     
 
-repeated = []
+unique_sequences = []
 
 x = len(sequences)
 for k in range(x):
-    if sequences[k] not in repeated:
-        repeated.append(sequences[k])
-    
-print(len(repeated))
+    if sequences[k] not in unique_sequences:
+        unique_sequences.append(sequences[k])
+        
+
+print(people)
+print(dna)
+
+
+print(len(unique_sequences))
 print(len(sequences))
 
         
