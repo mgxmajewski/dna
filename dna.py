25842dc8a5b1c5ee98996271c99e53dbd04f2885
mgxmajewski <mgxmajewski@gmail.com>
2021-01-26 22:52:13 +0000

    Splits reading people  database into two steps (as dictionary and list of sequences to search for

--- dna.py
+++ dna.py
@@ -2,43 +2,44 @@ import sys
 import csv
 
 
+# Read database of people as a dictionary
 people_database = sys.argv[1]
-
-# people = []
-
 with open (people_database, "r") as f:
-    sequence_reader = csv.reader(f)
-    dna_str = next(sequence_reader)[1:]
     people_reader = csv.DictReader(f)
     for row in people_reader:
         print (row)
 
+# Read dna STR which are input to look for
+with open (people_database, "r") as f:
+    sequence_reader = csv.reader(f)
+    dna_str = next(sequence_reader)[1:]
+
 print(dna_str)
 
-
+# Read DNA sequence as a string
 dna_material = sys.argv[2]
-
-
 with open (dna_material, "r") as f:
     dna = f.read()
 
 print(dna)    
 
-sequences = []
 
-i = 0
 
-for i in range (i, len(nucleotide), i+4):
-    sequences.append(nucleotide[i:i+4])
-    i += 1
+# sequences = []
+
+# i = 0
+
+# for i in range (i, len(nucleotide), i+4):
+#     sequences.append(nucleotide[i:i+4])
+#     i += 1
     
 
-unique_sequences = []
+# unique_sequences = []
 
-x = len(sequences)
-for k in range(x):
-    if sequences[k] not in unique_sequences:
-        unique_sequences.append(sequences[k])
+# x = len(sequences)
+# for k in range(x):
+#     if sequences[k] not in unique_sequences:
+#         unique_sequences.append(sequences[k])
         
 # print(sequences)
 
