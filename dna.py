4a7d864c41ea36629aed80f53851cfb561676899
mgxmajewski <mgxmajewski@gmail.com>
2021-01-30 22:21:18 +0000

    Refactor - initiate dictionary before comparing dna_srt's to dna

--- dna.py
+++ dna.py
@@ -25,6 +25,7 @@ with open(people_database, "r") as f:
     sequence_reader = csv.reader(f)
     dna_str = next(sequence_reader)[1:]
 
+print(dna_str)
 
 # Read DNA sequence as a string
 dna_material = sys.argv[2]
@@ -35,18 +36,32 @@ with open(dna_material, "r") as f:
 extracted_seq = {}
 seq_counter = 0
 
+for seq in range(len(dna_str)):
+    extracted_seq[dna_str[seq]] = 1
 
+    
 # Check how many dna_srt are there in dna 
 for seq in range(len(dna_str)):
-    seq_counter += 1
-    counter = 1
     for char in range(len(dna)):
         char_last = len(dna_str[seq])
         if dna_str[seq] == dna[char: char + char_last]:
-            extracted_seq[dna_str[seq]] = str(counter)
             if dna_str[seq] == dna[char + char_last: char + 2 * char_last]:
-                extracted_seq[dna_str[seq]] = str(counter)
-                counter += 1
+                extracted_seq[dna_str[seq]] += 1
+                    
+
+print(extracted_seq)
+
+# # Check how many dna_srt are there in dna 
+# for seq in range(len(dna_str)):
+#     seq_counter += 1
+#     counter = 1
+#     for char in range(len(dna)):
+#         char_last = len(dna_str[seq])
+#         if dna_str[seq] == dna[char: char + char_last]:
+#             extracted_seq[dna_str[seq]] = str(counter)
+#             if dna_str[seq] == dna[char + char_last: char + 2 * char_last]:
+#                 extracted_seq[dna_str[seq]] = str(counter)
+#                 counter += 1
                 
                 
 # Print result                
@@ -55,7 +70,8 @@ flag = False
 for row in people_list:
     temp = row['name'] 
     del row['name']
-    if row == extracted_seq:
+    print(row)
+    if row == str(extracted_seq):
         print(temp)
         flag = True
 
