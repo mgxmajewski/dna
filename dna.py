175c529eddfda0001535ea7b1b7eca5878bf9b05
mgxmajewski <mgxmajewski@gmail.com>
2021-01-31 00:45:11 +0000

    Rewriting dna_srt checking algorithm

--- dna.py
+++ dna.py
@@ -2,25 +2,25 @@ import sys
 import csv
 
  
-# Read database of people as a dictionary
-
-
+# Validate arguments provided by user
 if len(sys.argv) < 3:
     print("Provide 2 arguments")
     exit(1)
 
 
+# Read database of people as a dictionary
 people_database = sys.argv[1]
-
 
 with open(people_database, "r") as f:
     people_reader = csv.DictReader(f)
     people_list = list(people_reader)
         
+
             
-dna_str = []
 
 # Read dna STR which are input to look for
+dna_str = []
+
 with open(people_database, "r") as f:
     sequence_reader = csv.reader(f)
     dna_str = next(sequence_reader)[1:]
@@ -34,27 +34,19 @@ with open(dna_material, "r") as f:
 
 # Add dictionary to keep SRT count
 extracted_seq = {}
-seq_counter = 0
 
-for seq in range(len(dna_str)):
-    extracted_seq[dna_str[seq]] = 2
 
-str_found = []
-    
-# Check how many dna_srt are there in dna 
-for seq in range(len(dna_str)):
-    counter = 3
-    for char in range(len(dna)):
-        char_last = len(dna_str[seq])
-        if dna_str[seq] == dna[char: char + char_last]:
-            str_found.append(dna_str[seq])
-            if dna_str[seq] == dna[char + char_last: char + 2 * char_last]:
-                if dna_str[seq] == dna[char + 2* char_last: char + 3 * char_last]:
-                    extracted_seq[dna_str[seq]] = str(counter)
-                    counter += 1
-                    str_found.append(dna_str[seq])  
+# Check how many dna_srt are there in dna
 
-print(extracted_seq)
+longesr_srt_dna = []
+counter = 0
+
+for seq in dna_str:
+    print(seq)
+
+
+
+# print(extracted_seq)
 # # Check how many dna_srt are there in dna 
 # for seq in range(len(dna_str)):
 #     seq_counter += 1
@@ -67,7 +59,9 @@ print(extracted_seq)
 #                 extracted_seq[dna_str[seq]] = str(counter)
 #                 counter += 1
                 
-                
+        
+        
+# print(extracted_seq)
 # Print result                
 flag = False
 
